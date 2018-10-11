c0eb5ddf0a9a42d6a183865655573b7a5a905c72
canams <canams@hotmail.co.uk>
2018-10-11 16:55:20 +0100

    Create nav.php

---
+++ nav.php
@@ -0,0 +1,11 @@
+<h1> CD Catalogue </h1>
+
+	<ul class="nav">
+		<li><a class="<?php echo $home; ?>" href="home.php">Home</a></li>
+		<li><a class="<?php echo $artists; ?>" href="artists.php">Artists</a></li>
+		<li><a class="<?php echo $albums; ?>" href="albums.php">Albums</a></li>
+	</ul>
+
+	<div class="footer">
+  		<p>G51DBI Assignment - Chia</p>
+	</div>
